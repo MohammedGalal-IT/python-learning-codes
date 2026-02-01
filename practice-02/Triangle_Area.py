54b3689b0faf2172cc3357589a74b264684271b8
Mohammed Galal <MohammedGalal7777@hotmail.com>
2026-02-01 19:00:22 +0300

    Task #2 done

---
+++ practice-02/Triangle_Area.py
@@ -0,0 +1,9 @@
+print("Welcome to Triangle Area Calculator\n")
+
+base = float(input("Enter the base: "))
+height = float(input("Enter the height: "))
+base = print(f"\n\n Area = ({base}/2) * {height} = {0.5*base*height}")
+
+
+
+
