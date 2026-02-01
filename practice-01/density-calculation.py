783633a82fac4007b8a1da4129c550950d1c770e
Mohammed Galal <MohammedGalal7777@hotmail.com>
2026-02-01 17:29:41 +0300

    fix a stupid bug

--- practice-01/density-calculation.py
+++ practice-01/density-calculation.py
@@ -1,4 +1,4 @@
 mass = float(input("Enter the Mass: "))
 volume = float(input("Enter the volume: "))
 
-print("\n\n", "The Density of the object X = "+ str(mass) + "/" + str(volume) + " = " + str(mass*volume))
+print("\n\n", "The Density of the object X = "+ str(mass) + "/" + str(volume) + " = " + str(mass/volume))
